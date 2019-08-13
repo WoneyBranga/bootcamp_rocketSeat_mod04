bc54f92914a87bc3f11a0088c08389eea4bd688d
woneybranga <woney.branga@gmail.com>
2019-08-13 10:56:04 -0300

    06 - Class Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
-
-import profile from './assets/homer.gif'
-
+import TechList from './components/TechList';
 function App() {
-  return <img src={profile} width='200px'/>
+  return <TechList />
 }
 
 export default App;
